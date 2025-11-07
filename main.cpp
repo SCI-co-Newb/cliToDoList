97cb52b825fcc1fcf9f8d4b6385c5de9cff22296
Sahajdeep Singh <impulseVD@hotmail.com>
2025-11-06 22:01:48 -0500

    Initial Commit.

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+
+    // The first line/number of the file will be the total number of tasks. Compare with ranges to see if out of bounds.
+    // Look through the note app to see what to implement.
+    if (argc > 1) {
+        std::cout << "Hello, World!\n";
+    } else {
+        std::cout << "Goodbye, World!\n";
+    }
+
+    return 0;
+}
