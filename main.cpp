cf33a50daea6f9af5f0cff7193bee14e0e16b0a5
Sahajdeep Singh <impulseVD@hotmail.com>
2025-11-07 01:37:05 -0500

    Some planning done.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,31 @@
 #include <iostream>
 
-int main(int argc, char* argv[]) {
+// method when mode is r
+
+// method when mode is w
+
+// method when mode is u
+
+int main(const int argc, char* argv[]) {
 
     // The first line/number of the file will be the total number of tasks. Compare with ranges to see if out of bounds.
     // Look through the note app to see what to implement.
-    if (argc > 1) {
-        std::cout << "Hello, World!\n";
+    int totalTasks = 10;    // Here, you get the first number from the task file. It will be the total number of tasks.
+
+    if (argc < 1) {
+        std::cout << "No mode specified." << std::endl;
+        return 1;
+    }
+
+    if (strcmp(argv[1], "r") == 0) {
+        // check for corrections or errors, then call the method for r
+    } else if (strcmp(argv[1], "w") == 0) {
+        // check for corrections or errors, then call the method for w
+    } else if (strcmp(argv[1], "u") == 0) {
+        // check for corrections or errors, then call the method for u
     } else {
-        std::cout << "Goodbye, World!\n";
+        std::cout << "Invalid mode specified." << std::endl;
+        return 1;
     }
 
     return 0;
